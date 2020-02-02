806827d80165c7f0f8e7c3644d9b74c0af0c4c12
DouglasDao <douglas110394@gmail.com>
2020-02-02 22:49:14 +0530

    Splash Screen Completed

---
+++ lib/Splash.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() => runApp(Splash());
+
+class Splash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    debugPaintSizeEnabled = false;
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          primaryColor: Color(0xFF0A0E21),
+          scaffoldBackgroundColor: Color(0xFF0A0E21),
+        ),
+        home: Scaffold(
+          body: InitScreen(),
+        ));
+  }
+}
+
+class InitScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(12.0),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Center(
+              child: Text('Beautybliss by divs!', style: TextStyle(fontSize: 20.0)),
+            ), flex: 12,
+          ),
+          Expanded(
+            child: Text('Divya - Makeup & Hair!', style: TextStyle(fontSize: 11.0)),
+            flex: 1,
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+//void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+/*class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -108,4 +108,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-}
+}*/
