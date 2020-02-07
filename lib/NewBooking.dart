bb865594e4fbc6e2493328f3d8a092b1ce842428
Douglas <douglas@Srirams-MacBook-Pro-2.local>
2020-02-07 16:52:47 +0530

    New Bookings UI inprogress

--- lib/NewBooking.dart
+++ lib/NewBooking.dart
@@ -1,59 +1,103 @@
 import 'package:beautybliss/BeautyBlissUtils.dart';
 import 'package:flutter/material.dart';
 
-class NewBooking extends StatelessWidget {
+class NewBooking extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return NewBookingState();
+  }
+}
+
+class NewBookingState extends State<NewBooking> {
+  var _focusNode = FocusNode();
 
   final _bookingFormKey = GlobalKey<FormState>();
 
   @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(_focusListener);
+  }
+
+  void _focusListener(){
+    if(_focusNode.hasFocus){
+
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData(
-            primaryColor: Color(0xFF0A0E21),
-            scaffoldBackgroundColor: Color(0xFF0A0E21),
-            inputDecorationTheme: const InputDecorationTheme(
-              labelStyle: TextStyle(color: Colors.deepPurple),
-              hintStyle: TextStyle(color: Colors.grey),
-            )),
-        home: Scaffold(
+    return Scaffold(
 
-            appBar: AppBar(
-                leading: IconButton(
-                    icon: Icon(Icons.close, color: Colors.white),
-                    onPressed: () =>
-                        BeautyBlissUtils(mContext: context).finish()),
-                title: Text("New Booking"),
-                backgroundColor: Color(0xFF212121)),
+        appBar: AppBar(
+            leading: IconButton(
+                icon: Icon(Icons.close, color: Colors.white),
+                onPressed: () => BeautyBlissUtils(mContext: context).finish()),
+            title: Text("New Booking"),
+            backgroundColor: Color(0xFF212121)),
 
-            body: Form(
-                key: _bookingFormKey,
-                child: Padding(
+        body: Form(
+          key: _bookingFormKey,
+          child: Theme(
 
-                    padding: EdgeInsets.all(16.0),
-                    child: Column(
-                      children: <Widget>[
-                        TextFormField(
-                          cursorColor: Colors.deepPurple,
-                          decoration: InputDecoration(
-                            focusedBorder: UnderlineInputBorder(
-                               borderSide: BorderSide(color: Colors.deepPurple)
-                            ),
-                            contentPadding: EdgeInsets.all(8.0),
+              data: ThemeData.dark().copyWith(
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                hintColor: Colors.grey,
+                primaryColor: Colors.grey,
+                accentColor: Colors.lightBlue
+              ),
+
+              child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        focusNode: _focusNode,
+                        autocorrect: false,
+                        cursorColor: Colors.grey,
+                        keyboardType: TextInputType.text,
+                        textInputAction: TextInputAction.next,
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+
+                        decoration: InputDecoration(
+
                             filled: true,
+                            fillColor: Colors.white10,
+
                             hintText: 'Bride name',
                             labelText: 'Who is Bride ?',
-                          ),
-                        )
-                      ],
-                    )
-                )
-            )
+
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors.lightBlue)),
+
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors.transparent,
+                                    style: BorderStyle.none)),
+
+                            prefixIcon: Icon(Icons.person, color: Colors.grey)
+                        ),
+                      )
+                    ],
+                  )
+              )
+
+          ),
         )
     );
   }
 
-  void _hasText(String text){
-    if(text.isEmpty){
+  @override
+  void dispose() {
+    super.dispose();
+    _focusNode.removeListener(_focusListener);
+    _focusNode.unfocus();
+  }
+
+  void _hasText(String text) {
+    if (text.isEmpty) {
       SnackBar(content: Text("Field is mandatory!"));
     }
   }
